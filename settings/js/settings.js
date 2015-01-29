1c852cb864a59e9b4f90cfe846cbf0c579400cde
Andrey Drebot <a.drebot@gmail.com>
2015-01-29 11:23:16 +0200

    Chinese localization for template settings

--- settings/js/settings.js
+++ settings/js/settings.js
@@ -53,7 +53,7 @@
     allSupportedLanguages = allSupportedLanguages.sort(sortByName);
     allSupportedLanguages.push({
         key: "xx",
-        name: "Custom"
+        name: "自定义"
     });
 
     var viewModel = {
@@ -63,8 +63,8 @@
             data.enableXAPI = ko.observable(true),
 
             data.lrsOptions = [
-                { key: 'default', text: 'easygenerator (recommended)' },
-                { key: 'custom', text: 'Custom LRS' }
+                { key: 'default', text: 'easygenerator(推荐)' },
+                { key: 'custom', text: '自定义接收方式' }
             ];
 
             data.selectedLrs = ko.observable(data.lrsOptions[0].key);
@@ -142,19 +142,19 @@
 
             themes.list = [
                 {
-                    title: 'Cartoon light',
+                    title: '活力',
                     key: 'cartoon'
                 },
                 {
-                    title: 'Grey scheme',
+                    title: '灰底',
                     key: 'grey'
                 },
                 {
-                    title: 'Black scheme',
+                    title: '黑底',
                     key: 'black'
                 },
                 {
-                    title: 'Flat scheme',
+                    title: '沉稳',
                     key: 'flat'
                 }
             ];
@@ -559,7 +559,7 @@
         apiUrl: baseURL + '/storage/image/upload',
         maxFileSize: 10, //MB
         supportedExtensions: ['jpeg', 'jpg', 'png', 'bmp', 'gif'],
-        somethingWentWrongMessage: { title: 'Something went wrong', description: 'Please, try again' },
+        somethingWentWrongMessage: { title: '出错了', description: '请再试一遍' },
 
         status: {
             default: function () {
@@ -611,11 +611,11 @@
                 fileExtension = file.name.split('.').pop().toLowerCase();
 
             if ($.inArray(fileExtension, imageUploader.supportedExtensions) === -1) {
-                imageUploader.status.fail({ title: 'Unsupported image format', description: '(Allowed formats: ' + imageUploader.supportedExtensions.join(', ') + ')' });
+                imageUploader.status.fail({ title: '不支持的图片格式', description: '(支持的格式: ' + imageUploader.supportedExtensions.join(', ') + ')' });
                 return;
             }
             if (file.size > imageUploader.maxFileSize * 1024 * 1024) {
-                imageUploader.status.fail({ title: 'File is too large', description: '(Max file size: ' + imageUploader.maxFileSize + 'MB)' });
+                imageUploader.status.fail({ title: '文件太大', description: '(文件最大不超过: ' + imageUploader.maxFileSize + 'MB)' });
                 return;
             }
             imageUploader.uploadFile(file);
